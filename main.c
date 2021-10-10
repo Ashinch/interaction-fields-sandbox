d91a542a23fae40d8ea63461463c44f3a28225cd
Ash <glaxyinfinite@outlook.com>
2021-10-10 22:36:57 +0800

    Add resource monitoring

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <seccomp.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
 
 #include "main.h"
 
@@ -9,8 +12,10 @@
 int main(int argc, char **argv) {
     int opt, seccomp_is_open = 0;
     char *exe_path, *arg;
+    int max_cpu_time, max_real_time = 0;
+    long max_memory = 0;
 
-    while (EOF != (opt = getopt(argc, argv, "sb:p:"))) {
+    while (EOF != (opt = getopt(argc, argv, "sb:p:c:r:m:"))) {
         switch (opt) {
             case 's':
                 seccomp_is_open = 1;
@@ -21,8 +26,17 @@ int main(int argc, char **argv) {
             case 'p':
                 arg = optarg;
                 break;
+            case 'c':
+                max_cpu_time = atoi(optarg);
+                break;
+            case 'r':
+                max_real_time = atoi(optarg);
+                break;
+            case 'm':
+                max_memory = atol(optarg);
+                break;
             default:
-                return ARGUMENTS_ERROR;
+                return ARGUMENTS_FAILED;
         }
     }
     char **exe_argv = NULL;
@@ -30,64 +44,187 @@ int main(int argc, char **argv) {
         exe_argv = split(arg, ' ');
     }
 
-    scmp_filter_ctx ctx = NULL;
-    // Seccomp open
-    if (seccomp_is_open == 1) {
-        // System Call Blacklist
-        int black_list[] = {
-                SCMP_SYS(clone),
-                SCMP_SYS(fork),
-                SCMP_SYS(vfork),
-                SCMP_SYS(kill),
-                SCMP_SYS(socket)
-        };
+    struct result _result = {0, 0, 0, 0, 0, 0};
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
 
-        // Initialize seccomp
-        ctx = seccomp_init(SCMP_ACT_ALLOW);
-        if (!ctx) return INIT_SECCOMP_ERROR;
+    int pipefd[2];
+    if (pipe(pipefd) < 0) {
+        perror("Error creating pipe");
+        exit(EXIT_FAILURE);
+    }
 
-        // Add rules for black list
-        for (int i = 0, length = sizeof(black_list) / sizeof(int); i < length; i++) {
-            if (seccomp_rule_add(ctx, SCMP_ACT_KILL, black_list[i], 0) != 0) {
-                return ADD_BLACK_LIST_ERROR;
+    pid_t child_pid = fork();
+    if (child_pid < 0) {
+        // Fork failed
+        return FORK_FAILED;
+    } else if (child_pid == 0) {
+        // Child process
+        scmp_filter_ctx ctx = NULL;
+        if (seccomp_is_open == 1) {
+            // System Call Blacklist
+            int black_list[] = {
+                    SCMP_SYS(clone),
+                    SCMP_SYS(fork),
+                    SCMP_SYS(vfork),
+                    SCMP_SYS(kill),
+                    SCMP_SYS(socket)
+            };
+
+            // Initialize seccomp
+            ctx = seccomp_init(SCMP_ACT_ALLOW);
+            if (!ctx) return INIT_SECCOMP_FAILED;
+
+            // Add rules for black list
+            for (int i = 0, length = sizeof(black_list) / sizeof(int); i < length; i++) {
+                if (seccomp_rule_add(ctx, SCMP_ACT_KILL, black_list[i], 0) != 0) {
+                    return ADD_BLACK_LIST_FAILED;
+                }
+            }
+
+            // add extra rule for execve
+            if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(execve), 1,
+                                 SCMP_A0(SCMP_CMP_NE, (scmp_datum_t)(exe_path))) != 0) {
+                return ADD_BLACK_LIST_FAILED;
+            }
+
+            // do not allow "w" and "rw"
+            if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(open), 1,
+                                 SCMP_CMP(1, SCMP_CMP_MASKED_EQ, O_WRONLY, O_WRONLY)) != 0) {
+                return ADD_BLACK_LIST_FAILED;
+            }
+            if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(open), 1,
+                                 SCMP_CMP(1, SCMP_CMP_MASKED_EQ, O_RDWR, O_RDWR)) !=
+                0) {
+                return ADD_BLACK_LIST_FAILED;
+            }
+
+            // Loads the filter into the kernel
+            if (seccomp_load(ctx) != 0) {
+                return LOAD_BLACK_LIST_FAILED;
             }
         }
 
-        // add extra rule for execve
-        if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(execve), 1, SCMP_A0(SCMP_CMP_NE, (scmp_datum_t)(exe_path)))
-        !=
-        0) {
-            return ADD_BLACK_LIST_ERROR;
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        dup2(pipefd[1], STDERR_FILENO);
+
+        // Run the exe
+        execve(exe_path, exe_argv, NULL);
+
+        // Free
+        if (ctx != NULL) {
+            seccomp_release(ctx);
         }
 
-        // do not allow "w" and "rw"
-        if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(open), 1,
-                             SCMP_CMP(1, SCMP_CMP_MASKED_EQ, O_WRONLY, O_WRONLY)) != 0) {
-            return ADD_BLACK_LIST_ERROR;
+        if (exe_argv != NULL) {
+            for (int i = 0; *(arg + i); i++) {
+                free(*(exe_argv + i));
+            }
+            free(exe_argv);
         }
-        if (seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(open), 1, SCMP_CMP(1, SCMP_CMP_MASKED_EQ, O_RDWR, O_RDWR)) !=
-            0) {
-            return ADD_BLACK_LIST_ERROR;
+    } else if (child_pid > 0) {
+        // Oneself process
+        struct timeout_killer_args killer_args;
+        killer_args.timeout = max_real_time;
+        killer_args.pid = child_pid;
+        pthread_t tid = 0;
+        if (pthread_create(&tid, NULL, timeout_killer, (void *) (&killer_args)) != 0) {
+            kill(child_pid, SIGKILL);
+            return -1;
         }
 
-        // Loads the filter into the kernel
-        if (seccomp_load(ctx) != 0) {
-            return LOAD_BLACK_LIST_ERROR;
+        int status;
+        struct rusage resource_usage;
+
+        if (wait4(child_pid, &status, WSTOPPED, &resource_usage) == -1) {
+            kill(child_pid, SIGKILL);
+            return -1;
+        }
+
+        // get end time
+        gettimeofday(&end, NULL);
+        _result.real_time = (int) (end.tv_sec * 1000 + end.tv_usec / 1000 - start.tv_sec * 1000 - start.tv_usec / 1000);
+
+        close(pipefd[1]);
+        size_t size;
+        if (read_all(pipefd[0], &_result.result, &size) < 0) {
+            perror("Error reading");
+            _result.result = RUNTIME_ERROR.text;
+            _result.status_code = RUNTIME_ERROR.code;
+        } else {
+            fflush(stdout);
+            fflush(stderr);
+        }
+
+        // process exited, we may need to cancel timeout killer thread
+        if (max_real_time != -1) {
+            if (pthread_cancel(tid) != 0) {
+                // todo logging
+            }
+        }
+
+        if (WIFSIGNALED(status) != 0) {
+            _result.signal = WTERMSIG(status);
+        }
+
+        if (_result.signal == SIGUSR1) {
+            _result.result = SYSTEM_ERROR.text;
+            _result.status_code = SYSTEM_ERROR.code;
+        } else {
+            _result.cpu_time = (int) (resource_usage.ru_utime.tv_sec * 1000 +
+                                      resource_usage.ru_utime.tv_usec / 1000);
+            _result.memory = resource_usage.ru_maxrss * 1024;
+
+            _result.status_code = WEXITSTATUS(status) + 1;
+
+            if (_result.signal == SIGSEGV) {
+                if (max_memory != -1 && _result.memory > max_memory) {
+                    _result.result = MEMORY_LIMIT_EXCEEDED.text;
+                    _result.status_code = MEMORY_LIMIT_EXCEEDED.code;
+                } else {
+                    _result.result = RUNTIME_ERROR.text;
+                    _result.status_code = RUNTIME_ERROR.code;
+                }
+            } else {
+                if (_result.signal != 0) {
+                    _result.result = RUNTIME_ERROR.text;
+                    _result.status_code = RUNTIME_ERROR.code;
+                }
+                if (max_memory != -1 && _result.memory > max_memory) {
+                    _result.result = MEMORY_LIMIT_EXCEEDED.text;
+                    _result.status_code = MEMORY_LIMIT_EXCEEDED.code;
+                }
+                if (max_real_time != -1 && _result.real_time > max_real_time) {
+                    _result.result = REAL_TIME_LIMIT_EXCEEDED.text;
+                    _result.status_code = REAL_TIME_LIMIT_EXCEEDED.code;
+                }
+                if (max_cpu_time != -1 && _result.cpu_time > max_cpu_time) {
+                    _result.result = CPU_TIME_LIMIT_EXCEEDED.text;
+                    _result.status_code = CPU_TIME_LIMIT_EXCEEDED.code;
+                }
+            }
         }
     }
 
-    // Run the exe
-    execve(exe_path, exe_argv, NULL);
+    _result.result = escape_json(_result.result);
 
-    // Free
-    if (exe_argv != NULL) {
-        for (int i = 0; *(arg + i); i++) {
-            free(*(exe_argv + i));
-        }
-        free(exe_argv);
-    }
-    if (ctx != NULL) {
-        seccomp_release(ctx);
-    }
-    return 0;
+    printf("{\n"
+           "    \"cpuTime\": %d,\n"
+           "    \"realTime\": %d,\n"
+           "    \"memory\": %ld,\n"
+           "    \"signal\": %d,\n"
+           "    \"statusCode\": %d,\n"
+           "    \"result\": \"%s\"\n"
+           "}",
+           _result.cpu_time,
+           _result.real_time,
+           _result.memory,
+           _result.signal,
+           _result.status_code,
+           _result.result);
+
+    free(_result.result);
+
+    return _result.status_code == 1 ? 0 : -1;
 }
--- main.h
+++ main.h
@@ -8,13 +8,42 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <pthread.h>
+#include <signal.h>
+#include <regex.h>
 
 enum {
-    ARGUMENTS_ERROR         = -1,
-    INIT_SECCOMP_ERROR      = -2,
-    ADD_BLACK_LIST_ERROR    = -3,
-    LOAD_BLACK_LIST_ERROR   = -4,
+    ARGUMENTS_FAILED = -1,
+    FORK_FAILED = -2,
+    INIT_SECCOMP_FAILED = -3,
+    ADD_BLACK_LIST_FAILED = -4,
+    LOAD_BLACK_LIST_FAILED = -5,
 };
+
+struct timeout_killer_args {
+    int pid;
+    int timeout;
+};
+
+struct result {
+    int cpu_time;
+    int real_time;
+    long memory;
+    int signal;
+    int status_code;
+    char *result;
+};
+
+struct status_code {
+    char *text;
+    int code;
+};
+
+struct status_code CPU_TIME_LIMIT_EXCEEDED = {"CPU_TIME_LIMIT_EXCEEDED", 3};
+struct status_code REAL_TIME_LIMIT_EXCEEDED = {"REAL_TIME_LIMIT_EXCEEDED", 3};
+struct status_code MEMORY_LIMIT_EXCEEDED = {"MEMORY_LIMIT_EXCEEDED", 4};
+struct status_code RUNTIME_ERROR = {"RUNTIME_ERROR", 2};
+struct status_code SYSTEM_ERROR = {"SYSTEM_ERROR", 2};
 
 char **split(char *a_str, const char a_delim) {
     char **result = 0;
@@ -58,5 +87,165 @@ char **split(char *a_str, const char a_delim) {
 
     return result;
 }
+
+void *timeout_killer(void *timeout_killer_args) {
+    // this is a new thread, kill the process if timeout
+    pid_t pid = ((struct timeout_killer_args *) timeout_killer_args)->pid;
+    int timeout = ((struct timeout_killer_args *) timeout_killer_args)->timeout;
+    // On success, pthread_detach() returns 0; on error, it returns an error number.
+    if (pthread_detach(pthread_self()) != 0) {
+        kill(pid, SIGKILL);
+        return NULL;
+    }
+    // usleep can't be used, for time args must < 1000ms
+    // this may sleep longer that expected, but we will have a check at the end
+    if (sleep((unsigned int) ((timeout + 1000) / 1000)) != 0) {
+        kill(pid, SIGKILL);
+        return NULL;
+    }
+    if (kill(pid, SIGKILL) != 0) {
+        return NULL;
+    }
+    return NULL;
+}
+
+int read_all(int fd, char **buf_ptr, size_t *size_ptr) {
+    const size_t BLOCK_SIZE = 64 * 1024;
+
+    *size_ptr = 0;
+    *buf_ptr = NULL;
+
+    while (1) {
+        void *new_buf = realloc(*buf_ptr, *size_ptr + BLOCK_SIZE);
+        if (!new_buf) {
+            *buf_ptr = realloc(*buf_ptr, *size_ptr);  // Free unused space.
+            return 0;
+        }
+
+        *buf_ptr = new_buf;
+        char *p = ((char *) *buf_ptr) + *size_ptr;
+        ssize_t bytes_read = read(fd, p, BLOCK_SIZE);
+        if (bytes_read <= 0) {
+            *buf_ptr = realloc(*buf_ptr, *size_ptr);  // Free unused space.
+            return bytes_read;
+        }
+
+        *size_ptr += bytes_read;
+    }
+}
+
+int getFindStrCount(char *src, char *find) {
+    int count = 0;
+    char *position = src;
+    int findLen = strlen(find);
+    while ((position = strstr(position, find)) != NULL) {
+        count++;
+        position = position + findLen;
+    }
+    return count;
+}
+
+char *replaceAll(char *src, char *find, char *replaceWith) {
+    //如果find或者replace为null，则返回和src一样的字符串。
+    if (find == NULL || replaceWith == NULL) {
+        return strdup(src);
+    }
+    //指向替换后的字符串的head。
+    char *afterReplaceHead = NULL;
+    //总是指向新字符串的结尾位置。
+    char *afterReplaceIndex = NULL;
+    //find字符串在src字符串中出现的次数
+    int count = 0;
+    int i, j, k;
+
+    int srcLen = strlen(src);
+    int findLen = strlen(find);
+    int replaceWithLen = strlen(replaceWith);
+
+    //指向src字符串的某个位置，从该位置开始复制子字符串到afterReplaceIndex，初始从src的head开始复制。
+    char *srcIndex = src;
+    //src字符串的某个下标，从该下标开始复制字符串到afterReplaceIndex，初始为src的第一个字符。
+    int cpStrStart = 0;
+
+    //获取find字符串在src字符串中出现的次数
+    count = getFindStrCount(src, find);
+    //如果没有出现，则返回和src一样的字符串。
+    if (count == 0) {
+        return strdup(src);
+    }
+
+    //为新字符串申请内存
+    afterReplaceHead = afterReplaceIndex = (char *) malloc(srcLen + 1 + (replaceWithLen - findLen) * count);
+    //初始化新字符串内存
+    memset(afterReplaceHead, '\0', sizeof(afterReplaceHead));
+
+    for (i = 0, j = 0, k = 0; i != srcLen; i++) {
+        //如果find字符串的字符和src中字符串的字符是否相同。
+        if (src[i] == find[j]) {
+            //如果刚开始比较，则将i的值先赋给k保存。
+            if (j == 0) {
+                k = i;
+            }
+            //如果find字符串包含在src字符串中
+            if (j == (findLen - 1)) {
+                j = 0;
+                //拷贝src中find字符串之前的字符串到新字符串中
+                strncpy(afterReplaceIndex, srcIndex, i - findLen - cpStrStart + 1);
+                //修改afterReplaceIndex
+                afterReplaceIndex = afterReplaceIndex + i - findLen - cpStrStart + 1;
+                //修改srcIndex
+                srcIndex = srcIndex + i - findLen - cpStrStart + 1;
+                //cpStrStart
+                cpStrStart = i + 1;
+
+                //拷贝replaceWith字符串到新字符串中
+                strncpy(afterReplaceIndex, replaceWith, replaceWithLen);
+                //修改afterReplaceIndex
+                afterReplaceIndex = afterReplaceIndex + replaceWithLen;
+                //修改srcIndex
+                srcIndex = srcIndex + findLen;
+            } else {
+                j++;
+            }
+        } else {
+            //如果find和src比较过程中出现不相等的情况，则将保存的k值还给i
+            if (j != 0) {
+                i = k;
+            }
+            j = 0;
+        }
+    }
+    //最后将src中最后一个与find匹配的字符串后面的字符串复制到新字符串中。
+    strncpy(afterReplaceIndex, srcIndex, i - cpStrStart);
+
+    return afterReplaceHead;
+}
+
+
+char *strrepl(const char *src, char *dst, size_t dst_size, const char *search, const char *replace_with) {
+    char *replace_buf = (char *) malloc(dst_size);
+    if (replace_buf) {
+        replace_buf[0] = 0;
+        char *p = (char *) src;
+        char *pos = NULL;
+        while ((pos = strstr(p, search)) != NULL) {
+            size_t n = (size_t) (pos - p);
+            strncat(replace_buf, p, n > dst_size ? dst_size : n);
+            strncat(replace_buf, replace_with, dst_size - strlen(replace_buf) - 1);
+            p = pos + strlen(search);
+        }
+        snprintf(dst, dst_size, "%s%s", replace_buf, p);
+        free(replace_buf);
+    }
+    return dst;
+}
+
+char *escape_json(char *str) {
+    char tmp[200];
+    strncpy(tmp, str, 200);
+    tmp[199] = '\0';
+    return replaceAll(replaceAll(replaceAll(tmp, "\\", "\\\\"), "\"", "\\\""), "\n", "\\n");
+}
+
 
 #endif //INTERACTION_FIELDS_SANDBOX_MAIN_H
